0e4051027fb2d8c58f0c9a6e13405c7ee69b4970
wgarcia1011 <122066036+wgarcia1011@users.noreply.github.com>
2023-06-02 21:21:24 -0500

    commit

--- src/test/java/TestRunner/SmokeRunner.java
+++ src/test/java/TestRunner/SmokeRunner.java
@@ -9,7 +9,7 @@ import org.junit.runner.RunWith;
 @CucumberOptions(features = "src/test/resources/Features",
         glue = "StepDefinitions",
         dryRun = false,
-        tags = "@Test",
+        tags = "@smoke1",
         plugin = {"pretty", "html:target/Cucumber.html","json:target/Cucumber.json", "rerun:target/failed.txt"}
 
 )
